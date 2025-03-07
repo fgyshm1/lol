2cb6dce19501a4a25f0e9127ca242ba30bdd3c0f
fgyshm1 <lucas.su7@student.tdsb.on.ca>
2025-03-07 10:08:59 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Start Xfce and VNC
-export DISPLAY=:1
+# Start the Xfce desktop environment
+startxfce4 &
+
+# Start the VNC server
 tightvncserver :1 -geometry 1280x720 -depth 24
 
-# Start noVNC
-websockify --web=/usr/share/novnc/ --cert= --key= 6080 localhost:5901
+# Start the noVNC web proxy (with SSL certificates if you have them)
+mkdir -p /etc/ssl/novnc
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/ssl/novnc/novnc.key -out /etc/ssl/novnc/novnc.crt \
+  -subj "/CN=localhost"
+
+# Run noVNC to connect to the VNC server and serve the web interface on port 6080
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --cert=/etc/ssl/novnc/novnc.crt --key=/etc/ssl/novnc/novnc.key --web /usr/share/novnc/ --listen 6080
